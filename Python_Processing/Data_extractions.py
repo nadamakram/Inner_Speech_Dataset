e8d485e7f2484bf3e6bae5c49a867d59c97b714f
Nada Makram <nadamakram42@gmail.com>
2024-08-27 11:54:46 +0000

    feat(EDA): get inner only data

--- Python_Processing/Data_extractions.py
+++ Python_Processing/Data_extractions.py
@@ -11,7 +11,7 @@ import mne
 import gc
 import os
 import numpy as np
-from Utilitys import sub_name, unify_names       # noqa
+from .Utilitys import sub_name, unify_names       # noqa
 import pickle
 from mne.io import Raw
 
